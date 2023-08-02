6ae0d07cafc50833acab0d29ea5eff63016ae351
zhaojian <183105798372>
2023-08-02 15:36:46 +0800

    zhaojian use demo

--- NativeHook.js
+++ NativeHook.js
@@ -1,7 +1,8 @@
 // HOOK导出函数
 function Test01(){
-    let funGetFlag = Module.findExportByName("libphcm.so", "Java_com_ph0en1x_android_1crackme_MainActivity_getFlag");
-    send("native: " + funGetFlag);
+    // 用于在指定的模块中查找导出函数的地址
+    let funGetFlag = Module.findExportByName("libnative-lib.so", "Java_com_yijincc_ndkdemo_LoginActivity_login");
+    send("native: " + funGetFlag); // 基地址
     Interceptor.attach(funGetFlag, {
         onEnter: function(args){
             send("============getFlag===============");
@@ -23,11 +24,11 @@ function Test01(){
 // HOOK未导出函数
 function Test02(){
     // 绝对地址=so模块起始地址(基地址)+偏移地址
-    let baseAddr = Module.findBaseAddress("libphcm.so");
+    let baseAddr = Module.findBaseAddress("libnative-lib.so");
     send("baseAddr:"+baseAddr);
     // 指令集 分为ARM指令、thumb指令
     // ARM指令地址不变  thumb指令地址+1 sub_ 开头的函数 这种函数只能使用这种方式来进行
-    Interceptor.attach(baseAddr.add(0xFB9), {
+    Interceptor.attach(baseAddr.add(0x15F08), {
         onEnter: function(args){
             send("============encrypt===============");
             send(args[0]);
@@ -59,29 +60,36 @@ function Test02(){
 
 // HOOK枚举导入函数信息
 function Test03(){
-    let imports = Module.enumerateImportsSync("libphcm.so");
+    send("Test03")
+    let imports = Module.enumerateImportsSync("libnative-lib.so");
+    send(imports)
     for(let i=0;i<imports.length;i++){
-        if(imports[i].name.indexOf('raise') !== -1){
+        // if(imports[i].name.indexOf('raise') !== -1){
             send(imports[i]);
-        }
+        // }
     }
 }
 
 // HOOK枚举导出函数信息
 function Test04(){
-    let exports = Module.enumerateExportsSync("libphcm.so");
+    send("Test04-start")
+    send(device)
+    let exports = Module.enumerateExportsSync("libnative-lib.so");
+    send(exports)
     for(let i=0;i<exports.length;i++){
         if(exports[i].name.indexOf('Java_') !== -1){
             send("name:"+exports[i].name+"  address:"+exports[i].address);
         }
     }
+    send("Test04-end")
 }
 
 // 遍历模块列表信息
 function Test05(){
     Process.enumerateModules({
         onMatch: function(exp){
-            if(exp.name.indexOf('libphcm.so') !== -1){
+            send(exp)
+            if(exp.name.indexOf('libnative-lib.so') !== -1){
                 send('enumerateModules find');
                 send(exp);
                 return 'stop';
@@ -96,7 +104,8 @@ function Test05(){
 // 读写内存数据
 function Test06(){
     let mem_addr=Memory.alloc(20);
-    Memory.writeInt(mem_addr,0x12345678);
+    //Memory.writeInt(mem_addr,0x1234567890abcdef);
+    Memory.writeLong(mem_addr,0x1234567890abcdef);
     // console.log(hexdump(mem_addr));
     console.log(hexdump(mem_addr, {
         offset: 0,
@@ -108,7 +117,7 @@ function Test06(){
 
 // 使用frida api读写文件
 function Test07(){
-    let file = new File("/data/data/com.ph0en1x.android_crackme/yijincc.txt", "w");
+    let file = new File("/data/data/com.yijincc.ndkdemo/yijincc.txt", "w");
     file.write("hello world!!!\\n");
     file.flush();
     file.close();
@@ -117,30 +126,41 @@ function Test07(){
 // 基于主动调用libc.so里面的函数实现文件的读写操作
 function Test08(){
     let addr_fopen = Module.findExportByName("libc.so", "fopen");
+    send("1")
     let addr_fputs = Module.findExportByName("libc.so", "fputs");
+    send("2")
     let addr_fclose = Module.findExportByName("libc.so", "fclose");
+    send("3")
 
     let fopen = new NativeFunction(addr_fopen, "pointer", ["pointer", "pointer"]);
+    send("4")
     let fputs = new NativeFunction(addr_fputs, "int", ["pointer", "pointer"]);
+    send("5")
     let fclose = new NativeFunction(addr_fclose, "int", ["pointer"]);
+    send("6")
 
-    let filename = Memory.allocUtf8String("/data/data/com.ph0en1x.android_crackme/yijincc.txt");
+    let filename = Memory.allocUtf8String("/data/data/com.yijincc.ndkdemo/yijincc.txt");
+    send("7")
     let open_mode = Memory.allocUtf8String("w");
+    send("8")
     let file = fopen(filename, open_mode);
+    send("9")
 
     let buffer = Memory.allocUtf8String("hello world!!!\\n");
+    send("10")
     let result = fputs(buffer, file);
+    send("11")
     send("fputs:" + result);
     fclose(file);
 }
 
 Java.perform(function () {
-    Test01();
+    // Test01();
     // Test02();
     // Test03();
     // Test04();
     // Test05();
     // Test06();
     // Test07();
-    // Test08();
+    Test08();
 })
--- NativeHook.py
+++ NativeHook.py
@@ -14,9 +14,9 @@ def message(message, data):
 
 
 process = frida.get_remote_device().attach('NDKDemo')
-if not os.path.isfile('./JavaHook.js'):
-    raise TypeError("./JavaHook.js does not exist")
-with codecs.open('./JavaHook.js', 'r', 'UTF-8') as file:
+if not os.path.isfile('./NativeHook.js'):
+    raise TypeError("./NativeHook.js does not exist")
+with codecs.open('./NativeHook.js', 'r', 'UTF-8') as file:
     js_code = file.read()
 script = process.create_script(js_code)
 script.on("message", message)
@@ -26,12 +26,12 @@ sys.stdin.read()
 
 # frida使用非标准端口
 # /data/local/tmp # ./fs_12.7.22_arm64 -l 127.0.0.1:31928  默认端口: 27046
-# process = frida.get_device_manager().add_remote_device('127.0.0.1:31928').attach('FridaApp')
+# process = frida.get_device_manager().add_remote_device('127.0.0.1:31928').attach('Android_crackme')
 # if not os.path.isfile('./NativeHook.js'):
 #     raise TypeError("./NativeHook.js does not exist")
 # with codecs.open('./NativeHook.js', 'r', 'UTF-8') as file:
 #     js_code = file.read()
-# script = process.create_script(js_code)
+# script = process.create_script(js_code) # 创建脚本
 # script.on("message", message)
 # script.load()
 # sys.stdin.read()
